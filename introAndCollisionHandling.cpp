90f554788462eaf037eb53fdec668fc4ea8837b9
Procoder16 <smsoumik1608@gmail.com>
2021-07-21 01:08:32 +0530

    added the insert function without the rehash

--- introAndCollisionHandling.cpp
+++ introAndCollisionHandling.cpp
@@ -27,6 +27,17 @@ class HashTable{  // class to define the hash table
     int current_size;
     int table_size;
 
+    int hashFn(string key){
+        int idx = 0;
+        int p = 1;
+        for(int j = 0; j < key.length(); j++){
+            idx = idx + (key[j]*p)%table_size;
+            idx = idx % table_size;
+            p = (p*27)%table_size;
+        }
+        return idx;
+    }
+
     public:
     HashTable(int ts = 7){
         table_size = ts;
@@ -38,7 +49,14 @@ class HashTable{  // class to define the hash table
     }
 
     void insert(string key, T value){
+        int idx = hashFn(key);
+        Node<T> *n = new Node<T>(key, value);
+        // Insert at the head of the linked list with id = idx
+        n -> next = table[idx];
+        table[idx] = n;
+        current_size++;
 
+        //rehash
     }
 
     T search(string key){
