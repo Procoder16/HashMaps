77915c6b24a4c40f14c112328beec50c6e83904f
Procoder16 <smsoumik1608@gmail.com>
2021-07-21 01:47:25 +0530

    implemented the hashtable data structure and performed insert and print function

--- introAndCollisionHandling.cpp
+++ introAndCollisionHandling.cpp
@@ -1,74 +1,22 @@
-#include<iostream>
+#include<bits/stdc++.h> // standard header files are declared using <> 
+#include "hashtable.h" // user-defined header files are declared using "" 
+
+// here we are importing are self made header file class called hashtable.h
+
 using namespace std;
-
-template<typename T>  //using a template class
-class Node{ // this is a class to define the structure of the Node 
-
-    public:
-    string key;
-    T value;
-    Node<T>* next;
-
-    Node(string key, T val){
-        this-> key = key;
-        value = val;
-        next = NULL;
-    }
-    ~Node(){  // destructor so that when the node is deleted, it is called(would be a recursive approach)
-        if(next!=NULL){
-            delete next;
-        }
-    }
-};
-
-template<typename T>
-class HashTable{  // class to define the hash table 
-    Node<T>** table;
-    int current_size;
-    int table_size;
-
-    int hashFn(string key){
-        int idx = 0;
-        int p = 1;
-        for(int j = 0; j < key.length(); j++){
-            idx = idx + (key[j]*p)%table_size;
-            idx = idx % table_size;
-            p = (p*27)%table_size;
-        }
-        return idx;
-    }
-
-    public:
-    HashTable(int ts = 7){
-        table_size = ts;
-        table = new Node<T>*[table_size];
-        current_size = 0;
-        for(int i = 0; i < table_size; i++){
-            table[i] = NULL;
-        }
-    }
-
-    void insert(string key, T value){
-        int idx = hashFn(key);
-        Node<T> *n = new Node<T>(key, value);
-        // Insert at the head of the linked list with id = idx
-        n -> next = table[idx];
-        table[idx] = n;
-        current_size++;
-
-        //rehash
-    }
-
-    T search(string key){
-
-    }
-
-    void erase(string key){
-        
-    }
-
-};
 
 int main(){
 
+    HashTable<int> price_menu;
+    // here we are making an instance of the self defined data structure
+
+    price_menu.insert("Burger", 120);
+    price_menu.insert("Pepsi", 20);
+    price_menu.insert("BurgerPizza", 150);
+    price_menu.insert("Noodles", 25);
+    price_menu.insert("Coke", 40);
+
+    price_menu.print();
+
+    return 0;
 }
