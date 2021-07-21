a5ea4d74c13d0c7b3be1e10ed30001fa74cbe43a
Procoder16 <smsoumik1608@gmail.com>
2021-07-21 22:33:21 +0530

    added some more comments

--- introAndCollisionHandling.cpp
+++ introAndCollisionHandling.cpp
@@ -10,13 +10,13 @@ int main(){
     HashTable<int> price_menu;
     // here we are making an instance of the self defined data structure
 
-    price_menu.insert("Burger", 120);
+    price_menu.insert("Burger", 120); // inserting nodes in the hash table using the user defined function
     price_menu.insert("Pepsi", 20);
     price_menu.insert("BurgerPizza", 150);
     price_menu.insert("Noodles", 25);
     price_menu.insert("Coke", 40);
 
-    price_menu.print();
+    price_menu.print(); // calling the print function to print the hashtable
 
     return 0;
 }
