6bcb4587c0937f7e5504b4d3d7f674ef4027342d
Procoder16 <smsoumik1608@gmail.com>
2021-07-23 01:12:59 +0530

    search functionality added

--- phoneBookUM.cpp
+++ phoneBookUM.cpp
@@ -9,7 +9,7 @@ int main(){
 
     unordered_map<string, vector<string>> phonebook;
 
-    phonebook["Rahul"].push_back("9474");
+    phonebook["Rahul"].push_back("9474"); // adding the key with various values which is definitely going to get collided
     phonebook["Rahul"].push_back("9748");
     phonebook["Rahul"].push_back("9236");
     phonebook["Rahul"].push_back("9224");
@@ -19,12 +19,27 @@ int main(){
     phonebook["Kajal"].push_back("7567");
     phonebook["Kajal"].push_back("7000");
 
-    for(auto s : phonebook){
+    for(auto s : phonebook){ // looping through all the names
         cout<<s.first<<" -> ";
-        for(auto n : s.second){
+        for(auto n : s.second){ // looping thorugh alll the phone numbers of that partiular name
             cout<<n<<", ";
         }
         cout<<endl;
+    }
+
+    // search operation
+
+    string name;
+    cout<<"ENTER THE NAME TO SEARCH:";
+    cin>>name;
+
+    if(phonebook.count(name) == 0){ 
+        cout<<"ABSENT";
+    }
+    else{
+        for(string s : phonebook[name]){ // phonebook[name] gives you the values
+            cout<<s<<" ";
+        }
     }
 
     return 0;
