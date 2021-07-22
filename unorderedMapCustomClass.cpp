429f240cfd1e0e16a646c22de75fd863953489fe
Procoder16 <smsoumik1608@gmail.com>
2021-07-23 00:41:29 +0530

    added some more comments

--- unorderedMapCustomClass.cpp
+++ unorderedMapCustomClass.cpp
@@ -3,7 +3,7 @@
 #include<unordered_map>
 using namespace std;
 
-class Student{
+class Student{ // defining the class student as a used defined data type 
     public:
     string firstName;
     string lastName;
@@ -15,14 +15,16 @@ class Student{
         rollNo = rn;
     }
 
-    bool operator == (const Student &s) const{
+    bool operator == (const Student &s) const{ // operator overloading
+                                       // ???                    
         return rollNo == s.rollNo;
     }
 };
 
 class hashFn{
     public: 
-    size_t operator()(const Student &s) const{
+    size_t operator()(const Student &s) const{  // self defined hash function
+                                       // ???
         return s.firstName.length() + s.lastName.length();
     }
 };
@@ -31,12 +33,12 @@ int main(){
 
     unordered_map<Student, int, hashFn> student_map;
                                 //this is where you give the input of your custom hash function
-    Student s1("Soumik", "Mukherjee", "547");
+    Student s1("Soumik", "Mukherjee", "547"); // creating objects of the student class
     Student s2("Rohit", "Sharma", "045");
     Student s3("Soumik", "Ghosh", "078");
     Student s4("Rohit", "Sharma", "329");
 
-    student_map[s1] = 100;
+    student_map[s1] = 100; //mapping the keys to the values
     student_map[s2] = 97;
     student_map[s3] = 45;
     student_map[s4] = 88;
@@ -45,6 +47,7 @@ int main(){
 
     for(auto s : student_map){
         cout<<s.first.firstName<<" "<<s.first.lastName<<" and Marks:"<<s.second<<endl;
+        // make sure that they are a pair... so first and then firstName
     }   
 
     return 0;
