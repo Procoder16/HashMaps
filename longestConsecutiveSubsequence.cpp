1b0cfe1b0525370c0f8d3853e09d9270a73e01fb
Procoder16 <smsoumik1608@gmail.com>
2021-07-28 02:59:20 +0530

    code added

---
+++ longestConsecutiveSubsequence.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestConsecutiveSubsequence(int arr[], int n){
+    unordered_map<int, int> m; //number, its current streak length
+
+    for(int i = 0; i < n; i++){
+        int num = arr[i];
+        if(m.count(num - 1) == 0 && m.count(num + 1) == 0){
+            m[num] = 1;
+        }
+
+        else if(m.count(num - 1) && m.count(num + 1)){
+            int leftLen = m[num - 1];
+            int rightLen = m[num + 1];
+
+            int newLen = leftLen + 1 + rightLen;
+            m[num - leftLen] = newLen;
+            m[num + rightLen] = newLen;
+            m[num] = newLen;
+        }
+
+        else if(m.count(num - 1) && m.count(num + 1) == 0){
+            int strkLen = m[num - 1];
+            m[num - strkLen] = strkLen + 1;
+            m[num] = strkLen + 1;            
+        }
+
+        else if(m.count(num - 1) == 0 && m.count(num + 1)){
+            int strkLen = m[num + 1];
+            m[num + strkLen] = strkLen + 1;
+            m[num] = strkLen + 1;            
+        }
+    }
+
+    int largest = 0;
+    for(auto p : m){
+        largest = max(largest, p.second);
+    }
+
+    return largest;
+}
+
+int main(){
+
+    int arr[] = {14,5,1,2,6,3,7,8,9,13,5,11,12,13,17};
+
+    cout<<"LENGTH OF THE LONGEST CONSECUTIVE SUBSEQUENCE:"<<longestConsecutiveSubsequence(arr, 15);    
+
+    return 0;
+}
