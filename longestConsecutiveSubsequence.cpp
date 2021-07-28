1671cff890a9ada828d4c489d65da596a023d76c
Procoder16 <smsoumik1608@gmail.com>
2021-07-28 13:36:39 +0530

    documentation added

--- longestConsecutiveSubsequence.cpp
+++ longestConsecutiveSubsequence.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    -> The naive or the brute force approach would be just sorting the array and then find the consecutive numbers and then form a subsequence.
+       In this approach we would also have to manage the edge case of duplicate values as we only want unique elements in the subsequence.
+
+    -> The second approach is using a hash map(unordered_map) for this where we will map the number with the length of the current streak it belongs to.
+       The idea is that we would be traversing the array and for each element:
+            >> We would chech whether it has a streak starting from n + 1 or ending at n - 1.
+
+            Now there can be 4 cases:
+                -> It has no such streaks on either side. In that case just make a new pair with key as the number and value as 1
+                -> It has both these streaks:
+                    > Calculate the length of the left and the right streak respectively and then the new streak length would be a sum of the both + 1.
+                    > Then change the streak length of the left end and the right end
+                -> It has one streak only on any one side:
+                    > Calculate the streak length on that side.
+                    > Give the current number the value of streak length + 1
+                    > Change the end value to streak plus one also.             
+*/
+
 int longestConsecutiveSubsequence(int arr[], int n){
     unordered_map<int, int> m; //number, its current streak length
 
